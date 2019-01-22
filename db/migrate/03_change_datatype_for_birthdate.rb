1eae206be50e3b49508d692c1ac00ec73df1fe0a
Anuj Panchal <anujpan1@gmail.com>
2019-01-22 17:15:49 +0000

    Automatically backed up by Learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,6 +1,6 @@
 class ChangeDateTypeForBirthdate < ActiveRecord::Migration[5.1]
   
   def change
-    change_column :students, :birthdate, :string
+    change_column :students, :birthdate, :datetime
   end
 end
